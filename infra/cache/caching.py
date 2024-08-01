b3b65b30a3dbb6c0dfeefbb3c4ab9694c12a8c40
Diogo Silverio <dblsilverio+github@gmail.com>
2024-08-01 12:04:24 -0300

    Moved cache startup function and improve cache key generation

--- infra/cache/caching.py
+++ infra/cache/caching.py
@@ -3,13 +3,9 @@ import logging
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.inmemory import InMemoryBackend
 
-from main import app
-
-
 logger = logging.getLogger(__name__)
 
 
-@app.on_event("startup")
-async def startup():
+async def cache_startup():
     logger.info("Starting cache in-memory backend")
     FastAPICache.init(InMemoryBackend(), prefix="api-cache")
--- infra/cache/caching_keys.py
+++ infra/cache/caching_keys.py
@@ -1,7 +1,7 @@
 from typing import Optional
-from urllib.request import Request
 
 from fastapi_cache import FastAPICache
+from starlette.requests import Request
 from starlette.responses import Response
 
 
@@ -25,7 +25,6 @@ def function_kwargs_builder(*fields: str):
 
         final_kwargs = [f'{field}:{kwargs['kwargs'][field]}' for field in fields if field in kwargs['kwargs']]
         final_kwargs = ','.join(final_kwargs)
-
-        return f"{prefix}:{namespace}:{func.__module__}:{func.__name__}:{args}:{final_kwargs}"
+        return f"{prefix}:{namespace}:{func.__module__}:{func.__name__}:{args}:{final_kwargs}:{request.query_params}:{request.headers['accept']}"
 
     return key_builder
